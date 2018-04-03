0cf5a4eda4cf29902811ca7c204a7d342e0ffb8c
ridha rahmi <ridha.rahmi13@gmail.com>
2018-04-03 15:30:03 +0100

    update copy right footer

--- wp-content/themes/woocommerce/footer.php
+++ wp-content/themes/woocommerce/footer.php
@@ -1,4 +1,10 @@
-
+<footer>
+	<dir class="container">
+		<div class="copyright">
+			© 2018 - Developer By <a target="_black" href="http://www.facebook.com/ridha411">Ridha Rahmi </a> 
+		</div>
+	</dir>
+</footer>
     <?php wp_footer(); ?>
    </body>
  </html>
